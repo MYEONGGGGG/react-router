e47e15e8c08399466951f835ad3ac02485adc41d
myeongeun <c_me95@mentorit.co.kr>
2024-04-28 23:30:07 +0900

    react 버전에 맞춰 index.js 수정

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// 최신 버전, React 18 버전 이후(createRoot)
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// React 17 버전까지 이용 가능(ReactDOM.render)
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from "./reportWebVitals";
+//
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <App/>
+//     </BrowserRouter>,
+//     document.getElementById("root")
+// );
+
+// react 애플리케이션의 성능을 측정하고 감시하는 역할
 reportWebVitals();
